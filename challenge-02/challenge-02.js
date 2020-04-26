7d9bc11b3a0ae3d10c638fa4075d942d536fec12
Charllys Castro <charl@castro.tnisa.srv.local>
2020-04-26 01:42:36 -0300

    challenge-02

--- challenge-02/challenge-02.js
+++ challenge-02/challenge-02.js
@@ -1,32 +1,38 @@
-# Desafio da semana #2
+// # Desafio da semana #2
 
-Nesse exercício, você está livre para escolher os nomes para suas variáveis e funções! :smile:
+// Nesse exercício, você está livre para escolher os nomes para suas variáveis e funções! :smile:
 
-```js
+
 // Crie uma função que receba dois argumentos e retorne a soma dos mesmos.
-teste
+function soma(num1, num2){
+  return num1 + num2;
+}
 
 // Declare uma variável que receba a invocação da função criada acima, passando dois números quaisquer por argumento, e somando `5` ao resultado retornado da função.
-?
+var result = soma(3,2);
+result+= 5;
 
 // Qual o valor atualizado dessa variável?
-?
+result = 10;
 
 // Declare uma nova variável, sem valor.
-?
+var var2;
 
 /*
 Crie uma função que adicione um valor à variável criada acima, e retorne a string:
     O valor da variável agora é VALOR.
 Onde VALOR é o novo valor da variável.
 */
-?
+function adicionar(){
+  var2 = 10;
+  return 'O valor da variável agora é ' + var2;
+}
 
 // Invoque a função criada acima.
-?
+console.log(adicionar());
 
 // Qual o retorno da função? (Use comentários de bloco).
-?
+// O valor da variável agora é 10
 
 /*
 Crie uma função com as seguintes características:
@@ -35,19 +41,24 @@ Crie uma função com as seguintes características:
     Preencha todos os valores corretamente!
 3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação.
 */
-?
+function valor(num1, num2, num3){
+  if((num1 === undefined) || (num2 === undefined) || (num3 === undefined)){
+    return 'Preencha todos os valores correntamente';
+  }
+    return (num1*num2*num3) + 2;
+}
 
 // Invoque a função criada acima, passando só dois números como argumento.
-?
+var result = valor(1,2);
 
 // Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
-?
+// Preencha todos os valores correntamente
 
 // Agora invoque novamente a função criada acima, mas passando todos os três argumentos necessários.
-?
+result = valor(1,2,3);
 
 // Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
-?
+// 8
 
 /*
 Crie uma função com as seguintes características:
@@ -58,8 +69,23 @@ Crie uma função com as seguintes características:
 5. Se nenhum argumento for passado, retorne o valor booleano `false`.
 6. E ainda, se nenhuma das condições acima forem atendidas, retorne `null`.
 */
-?
+function arg(a, b, c){
+  if( a!== undefined && b === undefined && c === undefined){
+    return a;
+  }
+  else if(a!== undefined && b !== undefined && c === undefined){
+    return a + b;
+  }
+  else if(a !== undefined && b !== undefined && c !== undefined){
+    return (a+b)/c;
+  }
+  else if(a === undefined && b === undefined && c === undefined){
+    return false
+  }
+  else{
+    return null;
+  }
+}
 
 // Invoque a função acima utilizando todas as possibilidades (com nenhum argumento, com um, com dois e com três.) Coloque um comentário de linha ao lado da função com o resultado de cada invocação.
-?
-```
+console.log(arg())
